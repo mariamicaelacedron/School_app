9d6694f9ce864e69c24175526fa02ff9959935b8
Micaela Cedron <mikcedron@gmail.com>
2025-07-10 15:52:32 -0300

    Autocomplete users in the form

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -2,6 +2,6 @@ class Grade < ApplicationRecord
   belongs_to :user
   belongs_to :admin, class_name: "User"
 
-  validates :user_id, :subject, :score, :student_name, presence: true
+  validates :user_id, :subject, :score, presence: true
   validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
