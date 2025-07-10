f326b121895aec23106388e837129d979d875fe8
Micaela Cedron <mikcedron@gmail.com>
2025-07-10 16:14:51 -0300

    Add semesters in users

--- app/controllers/users/grades_controller.rb
+++ app/controllers/users/grades_controller.rb
@@ -3,7 +3,11 @@ module Users
     before_action :authenticate_user!
 
     def index
-      @grades = current_user.grades_received.includes(:admin)
+      @semesters = {
+        1 => current_user.grades_received.where(semester: 1).includes(:admin).order(created_at: :desc),
+        2 => current_user.grades_received.where(semester: 2).includes(:admin).order(created_at: :desc),
+        3 => current_user.grades_received.where(semester: 3).includes(:admin).order(created_at: :desc)
+      }
     end
   end
 end
