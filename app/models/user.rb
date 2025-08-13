ba2876892220f5d36cda02d5cacdb4b0174c5b16
Mica_cedron <111397263+mariamicaelacedron@users.noreply.github.com>
2025-08-13 03:25:03 -0300

    Profile (#27)
    * profile-update
    
    * Add delete in users
    
    * Add rspec binstub
    
    * Update ci.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,15 @@ class User < ApplicationRecord
   validate :avatar_content_type
   validates :name, presence: true, on: :update
   validate :avatar_size_and_type
+  validate :validate_avatar
 
-  has_many :grades
-  has_many :grades_received, class_name: "Grade", foreign_key: "user_id"
-  has_many :grades_assigned, class_name: "Grade", foreign_key: "admin_id"
-  has_many :summaries_received, class_name: "Summary", foreign_key: "user_id"
-  has_many :course_users
+  has_many :grades, dependent: :destroy
+  has_many :grades_received, class_name: "Grade", foreign_key: "user_id", dependent: :destroy
+  has_many :grades_assigned, class_name: "Grade", foreign_key: "admin_id", dependent: :nullify
+  has_many :summaries_received, class_name: "Summary", foreign_key: "user_id", dependent: :destroy
+  has_many :course_users, dependent: :destroy
   has_many :courses, through: :course_users
+  has_many :attendances, dependent: :destroy
 
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
@@ -23,6 +25,22 @@ class User < ApplicationRecord
     attachable.variant :large, resize_to_limit: [ 500, 500 ]
   end
   private
+  def validate_avatar
+    return unless avatar.attached?
+
+    # Validación de tamaño
+    if avatar.blob.byte_size > 5.megabytes
+      errors.add(:avatar, "es demasiado grande (máximo 5MB)")
+      avatar.purge # Elimina el archivo adjunto no válido
+    end
+
+    # Validación de tipo de contenido
+    acceptable_types = ["image/jpeg", "image/png", "image/gif"]
+    unless acceptable_types.include?(avatar.content_type)
+      errors.add(:avatar, "debe ser una imagen JPEG, PNG o GIF")
+      avatar.purge # Elimina el archivo adjunto no válido
+    end
+  end
 
   def self.permitted_params
     [ :name, :role, :email, :password, :password_confirmation, :invitation_token ]
